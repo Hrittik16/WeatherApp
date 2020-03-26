a41cf0e6f6f1b2cf7eecd2efd91c9c16cabba1c4
hrittik16 <hrittikdas16@gmail.com>
2020-03-26 22:17:54 +0530

    Updated

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import urllib.request
 import json
 
-def fahrenheitToCelcius(temperature):
-	celcius = (temperature-32)/1.8000
+def kelvinToCelcius(temperature):
+	celcius = temperature - 273.15
 	return celcius
 
-api_endpoint = "http://api.openweathermap.org/data/2.5/forecast"
+api_endpoint = "http://api.openweathermap.org/data/2.5/weather"
 city = "Kolkata"
 apikey = "7b09572fed2b8b1141cd8d2dc9b61270"
 #This is the url from where we will extract data
-url = api_endpoint + "?q=" + city + "&APPID=" + apikey  
+url = api_endpoint + "?q=" + city + "&appid=" + apikey  
 # urlopen function opens the url and stores it in response
 response = urllib.request.urlopen(url)
 # Store the json data
@@ -17,8 +17,9 @@ data = response.read()
 # Now, we will parse the json data to use it later
 parseResponse = json.loads(data)
 
-temperature = parseResponse['list'][0]['main']['temp']
-weather = parseResponse['list'][0]['weather'][0]['description']
+temperature = parseResponse['main']['temp']
+weather = parseResponse['weather'][0]['description']
 
-print("%.3f degree celcius" % (fahrenheitToCelcius(temperature)))
-print(city + " has a " + weather)
+print("Weather in " + city)
+print("%.2f degree celcius" % (kelvinToCelcius(temperature)))
+print("Weather Status : " + weather)
